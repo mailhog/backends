d0f591870774c80d040ace0960c854a78389c9e2
Ian Kent <email@iankent.co.uk>
2015-12-10 19:25:41 +0000

    refactor

--- config/config.go
+++ config/config.go
@@ -1,10 +1,25 @@
 package config
 
+import "errors"
+
 // BackendConfig defines an individual backend configuration
 type BackendConfig struct {
 	Type string                 `json:",omitempty"`
 	Ref  string                 `json:",omitempty"`
 	Data map[string]interface{} `json:",omitempty"`
+}
+
+// Resolve resolves a backend against a map of named backends
+func (b BackendConfig) Resolve(m map[string]BackendConfig) (BackendConfig, error) {
+	if len(b.Ref) == 0 {
+		return b, nil
+	}
+
+	if rb, ok := m[b.Ref]; ok {
+		return rb, nil
+	}
+
+	return b, errors.New("Backend not found")
 }
 
 // AppConfig defines the application configuration required by backends
